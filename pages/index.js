9684c8cf718aec4c72d9fc8a997397c066dc4fca
deepakmp444 <deepakmp444@gmail.com>
2022-07-05 22:36:56 +0530

    Link navigation

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
-const Home = ()=> {
-  return(
-    <h1> Home Page</h1>
-  )
-}
+import Link from "next/link";
+
+const Home = () => {
+  return (
+    <div>
+      <h1> Home Page</h1>
+      <Link href="/product">
+        <a>Product</a>
+      </Link>
+    </div>
+  );
+};
 
 export default Home;
--- pages/product/index.js
+++ pages/product/index.js
@@ -1,9 +1,37 @@
-function Product() {
+import Link from "next/link";
+function Product({ productId = 100 }) {
+
+// TODO: When we use "replace" then it totally replace =>  '/product' route then it goes in "/" route
+
   return (
     <div>
-      <h1>Product 1</h1>
-      <h1>Product 2</h1>
-      <h1>Product 3</h1>
+      <h3>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </h3>
+      <h1>
+        <Link href="/product/1">
+          <a> Product 1</a>
+        </Link>
+      </h1>
+      <h1>
+        <Link href="/product/2">
+          <a> Product 2</a>
+        </Link>
+      </h1>
+      <h1>
+        <Link href="/product/3" replace>
+          <a> Product 3</a>
+        </Link>
+      </h1>
+      <h1>
+        <Link href={`/product/${productId}`}>
+          <a> Product {productId}</a>
+        </Link>
+      </h1>
+      {/* <h1>Product 2</h1>
+      <h1>Product 3</h1> */}
     </div>
   );
 }
